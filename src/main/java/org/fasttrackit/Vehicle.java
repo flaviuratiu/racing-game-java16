8ccc519b77717c9cd300a89a65446e731a1dc733
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-06-04 21:10:53 +0300

    studied conditional statements

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -13,6 +13,20 @@ public class Vehicle {
     boolean damaged;
 
     public double accelerate(double speed, double durationInHours) {
+        if (speed > maxSpeed) {
+            System.out.println("Max speed exceeded.");
+            return 0;
+        } else if (speed == maxSpeed) {
+            System.out.println("Careful! Max speed reached!");
+        } else {
+            System.out.println("Valid speed entered.");
+        }
+
+        if (damaged) {
+            System.out.println("The vehicle is damaged. You cannot accelerate.");
+            return 0;
+        }
+
         System.out.println(make + " is accelerating with " + speed + " km/h for " + durationInHours + "h");
 
         double distance = speed * durationInHours;
@@ -24,6 +38,9 @@ public class Vehicle {
         System.out.println("Total traveled distance: " + totalTraveledDistance);
 
         double usedFuel = distance * mileage / 100;
+
+        // todo: use more fuel if speed > 120 km/h
+
         fuelLevel = fuelLevel - usedFuel;
 
         System.out.println("Remaining fuel: " + fuelLevel);
