edcc31fc148b84b56ce19f2502050112da292e9d
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-06-04 20:27:32 +0300

    added modulo example

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -80,5 +80,12 @@ public class App {
         // NPE (NullPointerException) example
 //        car2 = null;
 //        System.out.println(car2.make);
+
+//        System.out.println("Modulo (modulus) examples:");
+//        System.out.println(4 % 2);
+//        System.out.println(4 % 3);
+//
+//        System.out.println("Is 5 even?");
+//        System.out.println(5 % 2 == 0);
     }
 }
