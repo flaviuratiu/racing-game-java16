85a73bf82fc298c85db21418325a5319f4eb9385
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-06-25 21:30:42 +0300

    studied abstraction (abstract classes and interfaces)

---
+++ src/main/java/org/fasttrackit/domain/Mobile.java
@@ -0,0 +1,12 @@
+package org.fasttrackit.domain;
+
+public interface Mobile {
+
+    double accelerate(double speed, double durationInHours);
+
+    String getName();
+
+    boolean canMove();
+
+    double getTotalTraveledDistance();
+}
